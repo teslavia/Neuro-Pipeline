ae1307dcb403d0bcfed142c1c23ed98622749159
HackerMac <1194840395@qq.com>
2026-02-18 21:18:35 +0800

    chore: bump version to 2.2.2

--- rk3588-edge/include/neuro/core/constants.hpp
+++ rk3588-edge/include/neuro/core/constants.hpp
@@ -22,7 +22,7 @@ constexpr double kHealthUpdateIntervalSec = 5.0;
 constexpr double kFPSUpdateIntervalSec = 1.0;
 
 // Version
-constexpr const char* kEdgeVersion = "2.2.1";
+constexpr const char* kEdgeVersion = "2.2.2";
 
 }  // namespace neuro::core
 
